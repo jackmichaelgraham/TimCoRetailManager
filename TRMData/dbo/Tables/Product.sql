9bfe49e3bac3ea9c9677560229f76c22162485af
Jack Graham <jack.michael.graham@googlemail.com>
2020-03-25 13:23:37 +0000

    DB Tables

---
+++ TRMData/dbo/Tables/Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ProductName] NVARCHAR(50) NOT NULL,
+	[Description] NVARCHAR(MAX) NOT NULL,
+	[CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+	[LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
